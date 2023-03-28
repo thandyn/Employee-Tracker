8a54f6e06e13089585820f99da04a698869bd0ca
thandyn <nguyenphuthan@gmail.com>
2023-03-27 21:16:24 -0700

    updated index.js

--- index.js
+++ index.js
@@ -2,48 +2,50 @@ const db = require("./db");
 const inquirer = require("inquirer");
 require("console.table");
 
-mainPrompt() => {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "",
-      message: "What would you like to do?",
-      choices: [
-        {
-          name: "View all Departments.",
-          value: "VIEW_DEPARTMENTS"
-        },
-        {
-          name: "View all Roles.",
-          value: "VIEW_ROLES"
-        },
-        {
-          name: "View all Employees.",
-          value: "VIEW_EMPLOYEES"
-        },
-        {
-          name: "Add a Department.",
-          value: "ADD_DEPARTMENTS"
-        },
-        {
-          name: "Add a Role.",
-          value: "ADD_ROLE"
-        },
-        {
-          name: "Add an Employee.",
-          value: "ADD_EMPLOYEE"
-        },
-        {
-          name: "Update an Employee Role.",
-          value: "UPDATE_EMPLOYEE_ROLE"
-        },
-        {
-          name: "Quit",
-          value: "QUIT"
-        }
-      ]
-    }
-  ]).then(res => {
-    
-  })
-}
+const mainPrompt = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "answer",
+        message: "What would you like to do?",
+        choices: [
+          "View all Departments.",
+          "View all Roles.",
+          "View all Employees.",
+          "Add Departments.",
+          "Add Roles.",
+          "Add Employees.",
+          "Update Employee Role.",
+          "Quit",
+        ],
+      },
+    ])
+    .then((res) => {
+      switch (res.answer) {
+        case "View all Departments.":
+          viewDepartments();
+          break;
+        case "View all Roles.":
+          viewRoles();
+          break;
+        case "View all Employees.":
+          viewEmployees();
+          break;
+        case "Add Departments.":
+          addDepartments();
+          break;
+        case "Add Roles.":
+          addRoles();
+          break;
+        case "Add Employees.":
+          addEmployees();
+          break;
+        case "Update Employee Role":
+          updateEmployeeRoles();
+          break;
+        default:
+          quit();
+      }
+    });
+};
